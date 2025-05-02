788dca4a4508e94c50deaa748104a8becf87ab69
djhack29 <122092392+djhack29@users.noreply.github.com>
2025-05-02 01:35:20 +0100

    .

---
+++ app/admin/arch/page.tsx
@@ -0,0 +1,9 @@
+export default function Home(){
+    return(
+        <div className="h-full w-full flex justify-center items-center">
+            <div className="h-[70%] w-[70%] rounded-lg bg-blue-600">
+
+            </div>
+        </div>
+    )
+}
